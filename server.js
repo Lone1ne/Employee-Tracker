e6690652eb12ba7f4dde3e02ab47a36c13fda7a8
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-06-22 00:25:41 -0400

    Added inquirer prompt for list choices, added queries for view all departments, roles, and employees

--- server.js
+++ server.js
@@ -11,3 +11,49 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do? : ",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+    },
+  ])
+  .then((answers) => {
+    switch (answers.choice) {
+      case "View all departments":
+        db.query("SELECT * FROM department", (err, results) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(results);
+        });
+        break;
+      case "View all roles":
+        db.query("SELECT * FROM role", (err, results) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(results);
+        });
+        break;
+      case "View all employees":
+        db.query("SELECT * FROM employee", (err, results) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(results);
+        });
+        break;
+    }
+  });
