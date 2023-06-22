05b49276cc554a0f79e3b8cb36ccaaed7e7ed91d
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-06-22 01:43:11 -0400

    Added prompts for adding a new department, role, and employee. Updated 'View all roles' to join tables

--- server.js
+++ server.js
@@ -40,12 +40,18 @@ inquirer
         });
         break;
       case "View all roles":
-        db.query("SELECT * FROM role", (err, results) => {
-          if (err) {
-            console.log(err);
+        db.query(
+          `SELECT role.id, role.title, role.salary, department.name AS department 
+            FROM role 
+            JOIN department ON role.department_id = department.id`,
+          (err, results) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.table(results);
+            }
           }
-          console.table(results);
-        });
+        );
         break;
       case "View all employees":
         db.query("SELECT * FROM employee", (err, results) => {
@@ -54,6 +60,116 @@ inquirer
           }
           console.table(results);
         });
+        break;
+      case "Add a department":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "newDepartment",
+              message: "Enter the name of the new Department: ",
+            },
+          ])
+          .then((departmentResults) => {
+            db.query(
+              "INSERT INTO department(name)VALUES(?);",
+              departmentResults.newDepartment,
+              (err, results) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(
+                  `Sucess in adding the ${departmentResults.newDepartment} department to the department table`
+                );
+              }
+            );
+          });
+        break;
+      case "Add a role":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "roleTitle",
+              message: "Enter the role title: ",
+            },
+            {
+              type: "number",
+              name: "roleSalary",
+              message: "Enter the salary of the role: ",
+            },
+            {
+              type: "number",
+              name: "roleDepartmentId",
+              message: "Department ID: ",
+            },
+          ])
+          .then((roleResults) => {
+            db.query(
+              "INSERT INTO role(title, salary, department_id)VALUES(?, ?, ?);",
+              [
+                roleResults.roleTitle,
+                roleResults.roleSalary,
+                roleResults.roleDepartmentId,
+              ],
+              (err, results) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(
+                  `Sucess in adding the ${roleResults.roleTitle} role to the role table`
+                );
+              }
+            );
+          });
+        break;
+      case "Add an employee":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "employeeFirstName",
+              message: "Employee first name: ",
+            },
+            {
+              type: "input",
+              name: "employeeLastName",
+              message: "Employee last name: ",
+            },
+            {
+              type: "number",
+              name: "employeeRoleId",
+              message: "Role ID: ",
+            },
+            {
+              type: "number",
+              name: "employeeManagerId",
+              message: "Manager ID: ",
+            },
+          ])
+          .then((employeeResults) => {
+            employeeResults.employeeManagerId =
+              employeeResults.employeeManagerId
+                ? employeeResults.employeeManagerId
+                : null;
+            db.query(
+              "INSERT INTO employee(first_name, last_name, role_id, manager_id)VALUES(?, ?, ?, ?);",
+              [
+                employeeResults.employeeFirstName,
+                employeeResults.employeeLastName,
+                employeeResults.employeeRoleId,
+                employeeResults.employeeManagerId,
+              ],
+              (err, results) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(
+                  `Sucess in adding ${employeeResults.employeeFirstName} ${employeeResults.employeeLastName} to the employee table`
+                );
+              }
+            );
+          });
         break;
     }
   });
